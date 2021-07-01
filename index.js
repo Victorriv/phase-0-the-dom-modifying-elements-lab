7732a204876cb972a12e46f107f4536c5ad737cb
Victor <victorrivera714@yahoo.com>
2021-07-01 09:06:34 -0700

    commit tree 7d1ba6d78a06728b15b916d77959e1703f4d9ee5

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const newHeader = document.createElement('h1'); 
+newHeader.id = "victory"
+newHeader.innerHTML = "Victor is the champion"
+newHeader.className = "phrase"
+
+document.querySelector('main').remove();
+
